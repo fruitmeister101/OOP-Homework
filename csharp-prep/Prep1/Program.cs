97dd4da9905724873b53bbe2600d0fc2f61be96c
fruitmeister101 <fruitmeister101@gmail.com>
2023-04-20 18:42:44 -0600

    Bondulance Generator

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        AskName();
+    }
+
+
+    static void AskName(){
+        string[] name = new string[2];
+        
+        Console.Write("What is Your First Name? : ");
+        name[0] = Console.ReadLine();
+
+        Console.Write("What is your Last Name? : ");
+        name[1] = Console.ReadLine();
+
+        char[] temp2 = name[0].ToCharArray();
+        char[] temp3 = name[1].ToCharArray();
+
+        char temp1 = temp2[0]; // Save the first letter of one
+
+        temp2[0] = temp3[0]; // Swap one for the other
+        //temp3[0] = temp1; // Swap the other for the saved
+        temp3[0] = "N".ToCharArray()[0]; // Swap the other for the saved
+
+
+        Console.Write($"\n{name[1]}'s the name. {name[0]} name.\nPleasure to... wait?\n{name[1]} Name's the {name[0]}.\nAre you alright?\n{new string(temp2)} {new string(temp3)}'s having a Stronk. Call a {name[1]}ulance.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,27 @@ using System;
 
 class Program
 {
+    //public static int max = 50;
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        TestMethod(0);
+    }
+
+
+    static void TestMethod(int i)
+    {
+        int max = 50;
+        Console.WriteLine($"Hello hello? Testing testing. You have looped {i} times.");
+        
+        for (int j = 0; j < max; j++)
+        {
+            Console.WriteLine($"Hello hello? Testing testing. You have looped the second {i} times.");
+            
+        }
+        
+        if(i < max){
+            TestMethod(i + 1);
+        }
     }
 }
